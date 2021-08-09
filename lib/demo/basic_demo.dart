95338066791db17c9ad78633a463393ac8ca42f3
freebooterish <freebooter@263.net>
2021-08-09 14:56:06 +0800

    modified:   lib/demo/basic_demo.dart

--- lib/demo/basic_demo.dart
+++ lib/demo/basic_demo.dart
@@ -8,17 +8,58 @@ class BasicDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.grey[100],
+      // color: Colors.grey[100],
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: NetworkImage(
+                'http://resources.ninghao.org/images/say-hello-to-barry.jpg'),
+            alignment: Alignment.topCenter,
+            // repeat: ImageRepeat.repeatY,
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.indigoAccent[400]!.withOpacity(0.5),
+              BlendMode.hardLight,
+            )),
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
             child: Icon(Icons.pool, size: 32.0, color: Colors.white),
-            color: Color.fromRGBO(3, 54, 255, 1.0),
+            // color: Color.fromRGBO(3, 54, 255, 1.0),
             padding: EdgeInsets.all(16.0),
             margin: EdgeInsets.all(8.0),
             width: 80,
             height: 80,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(3, 54, 255, 1.0),
+              border: Border.all(
+                color: Colors.indigoAccent,
+                width: 3.0,
+                style: BorderStyle.solid,
+              ),
+              // borderRadius: BorderRadius.circular(16.0),
+              // BorderRadius.only(
+              //   topLeft: Radius.circular(16.0),
+              //   bottomRight: Radius.circular(16.0),
+              // ),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(6.0, 7.0),
+                  color: Color.fromRGBO(16, 20, 188, 1.0),
+                  // 阴影的模糊程度
+                  blurRadius: 20.0,
+                  spreadRadius: 3.0,
+                ),
+              ],
+              shape: BoxShape.circle,
+              gradient: RadialGradient(
+                colors: [
+                  Color.fromRGBO(7, 102, 255, 1.0),
+                  Color.fromRGBO(3, 28, 128, 1.0),
+                ],
+              ),
+            ),
           ),
         ],
       ),
